0eee16f25ef4dc3f6fb2c4b7d4c37745854ee0cd
SAFIKUL ALAM <116943326+safikulalam96@users.noreply.github.com>
2024-01-27 07:33:12 +0000

    custom cursor

---
+++ projects/stylish_cursor/script.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('#main');
+const cursor = document.querySelector('#cursar');
+
+
+main.addEventListener("mousemove", (dets) => {
+    cursor.style.left = dets.x + "px";
+    cursor.style.top = dets.y + "px";
+})
